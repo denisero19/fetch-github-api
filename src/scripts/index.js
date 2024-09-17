36b9d7deb16a27545efa3eff396270651439088e
Denise Rodrigues <deniseroav7@gmail.com>
2024-09-17 16:19:56 -0300

    modified

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
+import { getUser } from "./services/user.js"
+import { getRepositories } from "./services/repositories.js"
 
-import { getRepositories } from "/src/scripts/services/repositories.js"
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
 
-import { user } from "/src/scripts/objects/user.js"
-import { screen } from "/src/scripts/objects/screen.js"
-import { getUser } from "/src/scripts/services/user.js"
 document.getElementById('btn-search').addEventListener('click', () =>{
     const userName = document.getElementById('input-search').value
     if(validateEmptyInput(userName)) return
